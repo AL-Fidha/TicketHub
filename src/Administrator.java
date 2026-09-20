44a6fc103954439a8b49a2c4908124a32689ad32
AL-Fidha <alfidhasr@example.com>
2026-09-20 19:53:14 +0530

    Added Administrator.java

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Administrator extends User {
 
     public Administrator(String userId, String username, String password) {
@@ -9,9 +11,11 @@ public class Administrator extends User {
         System.out.println("1. Manage Movies\n2. View Reports\n3. Manage Bookings");
     }
 
-    public void addMovie(Movie movie) {
-        System.out.println("Movie added: " + movie.getTitle());
-    }
+    public void addMovie(Movie movie, List<Movie> catalog) {
+    catalog.add(movie);
+    System.out.println("Movie added: " + movie.getTitle());
+}
+
 
     public void removeMovie(Movie movie) {
         System.out.println("Movie removed: " + movie.getTitle());
