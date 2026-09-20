212d59d256866489730267d48baff6c37b239c4b
AL-Fidha <alfidhasr@example.com>
2026-09-20 19:45:58 +0530

    Added Administrator.java

---
+++ src/Administrator.java
@@ -0,0 +1,20 @@
+public class Administrator extends User {
+
+    public Administrator(String userId, String username, String password) {
+        super(userId, username, password, "Administrator");
+    }
+
+    @Override
+    public void showMenu() {
+        System.out.println("1. Manage Movies\n2. View Reports\n3. Manage Bookings");
+    }
+
+    public void addMovie(Movie movie) {
+        System.out.println("Movie added: " + movie.getTitle());
+    }
+
+    public void removeMovie(Movie movie) {
+        System.out.println("Movie removed: " + movie.getTitle());
+    }
+}
+
